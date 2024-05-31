6485d03e38d1f9c4395f9d2c6318c2af66fcb07d
Divani Perera <divamanashvi03@gmail.com>
2024-05-31 13:05:23 +0530

    Add project files.

---
+++ pet-backend/Entities/ProductEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace pet_backend.Entities
+{
+    public class ProductEntity
+    {
+        public int Id { get; set; }
+
+        public string Brand { get; set; }
+         public string Title { get; set; }
+
+        public DateTime CreatedAt  { get; set; }
+
+        public DateTime UpdatedAt { get; set;}
+    }
+}
